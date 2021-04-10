a35152f666e5e3e16877d2d289d2d00cba8d6c48
Your NameLinzhouLi <LinzhouLi@outlook.com>
2021-04-10 16:00:14 +0800

    image SVD transformation

--- SVD.py
+++ SVD.py
@@ -2,11 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mping
 
-#图片单色彩通道进行矩阵SVD处理，取前n项
-def image_svd_single_color(img_s,n):
-    U,sigma,D = np.linalg.svd(img_s)
+def image_svd(img):
+    r = img[:,:,0]
+    g = img[:,:,1]
+    b = img[:,:,2]
+    Ur,Sr,Dr = np.linalg.svd(r)
+    Ug,Sg,Dg = np.linalg.svd(g)
+    Ub,Sb,Db = np.linalg.svd(b)
+    return Ur,Sr,Dr,Ug,Sg,Dg,Ub,Sb,Db
+
+def USD(U, sigma, D, m, n):
     S = np.zeros((U.shape[0], D.shape[1]))
-    for i in range(n):
+    for i in range(m - 1, n):
         S[i,i] = sigma[i]
     U_S = np.matmul(U, S)
     img_s_svd = np.matmul(U_S, D)
@@ -15,14 +22,11 @@ def image_svd_single_color(img_s,n):
     img_s_svd = img_s_svd.astype(np.uint8)
     return img_s_svd
 
-#图片SVD分解
-def image_svd(img, n):
-    r = img[:,:,0]
-    g = img[:,:,1]
-    b = img[:,:,2]
-    r_svd = image_svd_single_color(r, n)
-    g_svd = image_svd_single_color(g, n)
-    b_svd = image_svd_single_color(b, n)
+def image_decompose(img, m, n):
+    Ur,Sr,Dr,Ug,Sg,Dg,Ub,Sb,Db = image_svd(img)
+    r_svd = USD(Ur, Sr, Dr, m, n)
+    g_svd = USD(Ug, Sg, Dg, m, n)
+    b_svd = USD(Ub, Sb, Db, m, n)
     img_svd = np.stack((r_svd, g_svd, b_svd), axis = 2)
     return img_svd
 
@@ -31,6 +35,6 @@ if __name__ == '__main__':
     img = mping.imread(img_path)
     max = min(img.shape[0], img.shape[1])
     n = int(input("输入奇异值项数(1~%d): "%(max)))
-    pic = image_svd(img, n)
+    pic = image_decompose(img, 1, n)
     plt.imshow(pic)
     plt.show()
